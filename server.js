fbdca0336eb23891bab7b7ad35a3f0e1d5d752aa
Alberto Perez <331258@NTTDATA.COM>
2025-01-22 13:45:35 -0600

    new configs in vercel.json port

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.post("/create-payment-intent", async (req, res) => {
   }
 });
 
-app.listen(5252, () =>
+app.listen(process.env.PORT || 3000, () =>
   console.log(`Node server listening at http://localhost:5252`)
 );
